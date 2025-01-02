721d88d79342fcd577c43322cc035ddb2d58240b
mswomley <maggieswomley18@gmail.com>
2025-01-01 20:02:28 -0500

    back to basic site

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Routes, Link} from "react-router-dom";
-import Home from "./Home"
-import Projects from "./Projects"
+
 
 function App () {
   return (
-<Router>
-      <nav class="topnav">
-        <Link class="topnav" to="/">Home</Link>
-        <Link class="topnav" to="/projects">Projects</Link>
-      </nav>
-  <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-  </Routes>
+
   <footer className="contact">
       <hr></hr>
       CONTACT<br></br>
@@ -22,7 +12,6 @@ function App () {
       <a href="https://www.linkedin.com/in/maggie-s-5427491a2/"><button className="button" >LinkedIn</button></a><br></br>
       <a href="https://github.com/mswomley"><button className="button" >Github</button></a>
       </footer>
-</Router>
   )
 }
 
