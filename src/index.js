da63dc071e61ac8acc38cb748871f7466d714f0b
mswomley <maggieswomley18@gmail.com>
2025-01-01 20:16:50 -0500

    setting up routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Art from "./pages/Art.js";
+import Home from "./pages/Home.js";
+import Layout from "./pages/Layout.js"
+import NoPage from "./pages/NoPage.js"
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+     <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="art" element={<Art />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+root.render(<App />);
---
+++ src/pages/Art.js
@@ -0,0 +1,5 @@
+const Art = () => {
+    return <h1>Art Page</h1>;
+  };
+  
+  export default Art;
