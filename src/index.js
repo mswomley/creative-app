1792a43429e978736c6a5103b88fab4fa0e1ed47
mswomley <maggieswomley18@gmail.com>
2025-01-05 17:36:01 -0500

    taking down skunk page for now

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Mouse from "./pages/mouse.js";
 import Home from "./pages/Home.js";
 import NoPage from "./pages/NoPage.js"
 import Bear from "./pages/bear.js"
-import Skunk from "./pages/skunk.js"
+// import Skunk from "./pages/skunk.js"
 
 
 export default function App() {
@@ -16,13 +16,11 @@ export default function App() {
         <Link class="topnav" to="/">bunny</Link>
         <Link class="topnav" to="/mouse">mouse</Link>
         <Link class="topnav" to="/bear">bear</Link>
-        <Link class="topnav" to="/skunk">skunk</Link>
       </nav>
      <Routes>
      <Route path="/" element={<Home />} />
       <Route path="/mouse" element={<Mouse />} />
       <Route path="/bear" element={<Bear />} />
-      <Route path="/skunk" element={<Skunk />} />
       <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
